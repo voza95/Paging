d48b7421047109796e743b4e29db2235cdb5568f
Vaibhav Oza <35027648+voza95@users.noreply.github.com>
2020-11-25 22:19:55 +0530

    First Commit

---
+++ app/src/main/java/com/example/paging/GitRepoDataSourceFactory.kt
@@ -0,0 +1,15 @@
+package com.example.paging
+
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.DataSource
+
+class GitRepoDataSourceFactory : DataSource.Factory<Int, GitRepo>() {
+
+    val gitRepoLiveDataSource = MutableLiveData<GitRepoDataSource>()
+
+    override fun create(): DataSource<Int, GitRepo> {
+        val repoDataSource = GitRepoDataSource()
+        gitRepoLiveDataSource.postValue(repoDataSource)
+        return repoDataSource
+    }
+}
